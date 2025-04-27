e05fe5c7363db3f053cfcdf563e02329c220c3be
arya-anathema <183879173+arya-anathema@users.noreply.github.com>
2025-04-27 18:15:07 -0400

    minor updates + kfold vaildation

--- ml.py
+++ ml.py
@@ -1,38 +1,44 @@
-import pandas as pd
+import pandas as pd 
 from sklearn.linear_model import LogisticRegression, RidgeClassifier
-from sklearn.model_selection import train_test_split, TunedModelClassifierCV
+from sklearn.model_selection import train_test_split,cross_validate
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score, classification_report, f1_score, make_scorer
+from sklearn.metrics import accuracy_score, classification_report, f1_score
 
-#import csv as 2d array
 
 data = pd.read_csv("formatted_data.csv")
 
 X = data[["Packet_Count","Total_Length","Average_Packet_Interval","Maximum_Packet_Interval","Minimum_Packet_Interval","Average_Packet_Length","Maximum_Packet_Length","Minimum_Packet_Length","Most_Common_Packet_Length"]]
-y = data['Label'].map({'youtube': 0, 'reddit': 1,'github': 2, 'weather': 3, 'wikipedia': 4,'bestbuy': 5})
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+y = data['Label'].map({'youtube': 0, 'reddit': 1,'github': 2, 'honda': 3, 'wikipedia': 4,'bestbuy': 5})
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
 
 #split data using train_test_split
     
 def model_train(model):
     
+    scores = cross_validate(model, X = X_train, y = y_train,cv = 4, scoring = ["f1_macro","accuracy"],return_estimator=True)
+    print('Best F1 Score: ',max(scores['test_f1_macro']),'\nBest Accuracy: ',max(scores['test_accuracy']))
+    print('Average F1 Score: ',scores['test_f1_macro'].mean(),'\nAverage Accuracy: ',scores['test_accuracy'].mean())
+    print(scores['test_f1_macro'],'\n',scores['test_accuracy'])
 
-    model.fit(X_train,y_train)
+    df = pd.DataFrame(columns=['f1_macro','accuracy'])
 
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f'Accuracy: {accuracy:.2f}')
-    print('Classification Report:')
-    print(classification_report(y_test, y_pred))
 
-def model_tuning():
-    return
+    for model in scores['estimator']:
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test,y_pred)
+        f1_macro = f1_score(y_test,y_pred,average = 'macro')
+        df = pd.concat([df,pd.DataFrame(data = {'f1_macro':[f1_macro],'accuracy':[accuracy]})])
+
+    
+    print('Max Prediction Macro F1: ',df.f1_macro.max(),'\nMax Prediction Accuracy: ',df.accuracy.max())
+    print('Mean Prediction Macro F1: ',df.f1_macro.mean(),'\nMean Prediction Accuracy: ',df.accuracy.mean())
+
+
 
 modelGBC = GradientBoostingClassifier()
-modelLogisticReg = LogisticRegression()
 modelRidge = RidgeClassifier()
 
 model_train(modelGBC)
-#model_train(modelLogisticReg)
-#model_train(modelRidge)
+model_train(modelRidge)
 
